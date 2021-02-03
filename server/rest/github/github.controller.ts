3e986093dfe95402aeff77b3781e54859727cc2f
again7536 <45936642+again7536@users.noreply.github.com>
2021-02-03 13:15:01 +0900

    [Fix] auto build

--- server/rest/github/github.controller.ts
+++ server/rest/github/github.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Post} from '@nestjs/common';
+import { Controller, Post, Req} from '@nestjs/common';
 import { GithubService } from './github.service';
+import { Request } from 'express';
 
 @Controller('github')
 export class GithubController {
     constructor(private githubService:GithubService) {}
 
     @Post()
-    public async pushed():Promise<boolean> {
-        return await this.githubService.autoBuild();
+    public async pushed(@Req() req:Request):Promise<boolean> {
+        return await this.githubService.autoBuild(req);
     }
 }
--- server/rest/github/github.service.ts
+++ server/rest/github/github.service.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@nestjs/common';
-import { exec } from 'child_process';
+import { Request } from 'express';
+import { promisify } from 'util';
+import crypto from 'crypto';
 
 @Injectable()
 export class GithubService {
 
-    async autoBuild():Promise<boolean> {
-        try {
-            exec('git pull && npm run build && npm start', ()=>console.log('builded'));
-            return true;
-        } catch (err) {
-            console.log(err);
+    private verifyPostData (req:Request) :boolean {
+        const secret = 'wlrmadms159';
+        // GitHub: X-Hub-Signature
+        // Gogs:   X-Gogs-Signature
+        const sigHeaderName = 'X-Hub-Signature'
+        const payload = JSON.stringify(req.body)
+        if (!payload) {
+          return false;
+        }
+        
+        const sig = req.get(sigHeaderName) || ''
+        const hmac = crypto.createHmac('sha1', secret)
+        const digest = Buffer.from('sha1=' + hmac.update(payload).digest('hex'), 'utf8')
+        const checksum = Buffer.from(sig, 'utf8')
+        if (checksum.length !== digest.length || !crypto.timingSafeEqual(digest, checksum)) {
+          return false;
+        }
+        return true;
+    }
+
+    async autoBuild( req:Request ):Promise<boolean> {
+        const exec = promisify(require('child_process').exec);
+        
+        if(this.verifyPostData(req) === true) {
+            try{
+                const {stdout, stderr} = await exec('git pull && npm run build && npm start');
+                console.log(stdout);
+                console.error(stderr);
+                return true;
+            } catch (err) {
+                console.error(err);
+                return false;
+            }
+        }
+        else {
             return false;
         }
     }
