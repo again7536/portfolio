a2749dc8a7f360ef599a554dfa2dff8627e78a21
again7536 <45936642+again7536@users.noreply.github.com>
2021-02-03 14:05:56 +0900

    [Fix] auto build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <ul className='projects'>
                 <li><Link href="/autumn">Autumn</Link></li>
             </ul>
-            hiaa22:
+            hiaatest:
             {process.env.NODE_ENV}
         </div>
     );
