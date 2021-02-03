95c8530393cf9850eabcb2c156d7bd0ec8d973e6
again7536 <45936642+again7536@users.noreply.github.com>
2021-02-03 12:43:53 +0900

    [Fix] auto build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <ul className='projects'>
                 <li><Link href="/autumn">Autumn</Link></li>
             </ul>
-            hi
+            hihi:
             {process.env.NODE_ENV}
         </div>
     );
