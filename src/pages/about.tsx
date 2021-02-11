7ceb5c1a3476ccd9953cce9e59a049a3375c23e4
again7536 <45936642+again7536@users.noreply.github.com>
2021-02-11 14:01:07 +0900

    [Fix] contact page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from '../styles/pages/about.module.scss';
+
+import Navbar from '../components/home/navbar';
 
 export default function About():JSX.Element {
 
     return (
         <>
+        <Navbar/>
+        <div className={styles.about}>
+            <div className={styles.image}></div>
+        </div>
         </>
     );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styles from '../styles/pages/contact.module.scss';
+import Navbar from '../components/home/navbar';
 
 export default function Contact() {
     return (
-        <div>
+        <>
+        <Navbar/>
+        <div className={styles.contact}>
+            <br/><br/>
             <a href='https://www.freepik.com/vectors/abstract'>
                 Abstract vector created by vectorjuice - www.freepik.com
             </a>
         </div>
+        </>
     )
 }
